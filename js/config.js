3a9dea738871be2d596b442ec2cb944f6af85dc6
eo01-programas <cofacoeo01@gmail.com>
2026-04-07 09:44:27 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-﻿const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby9elh9dctolsPIQYUY2ORT7JnP10AInidx1OOON02omhkjRXhZpICOfdDAh7mP_35G/exec";
-
-window.APP_CONFIG = {
-    WEB_APP_URL,
-};
